c4946865d51f32cac5d4cc20cf6384208000c10a
PSilling <petr.silling@centrum.cz>
2017-02-26 21:17:29 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'antd';
+import { Form } from 'antd';
+import { Table } from 'antd';
+import { Icon } from 'antd';
+import titleimg from './titleimg.png'; //source: http://www.clker.com/cliparts/a/1/3/8/11949859511240324938open_book_nae_02.svg.hi.png
 import './App.css';
+
+const data = [{
+    key: 1,
+    id: `Edward King`,
+    date: 32,
+    title: `London, Park Lane no.`,
+    action: 'Delete',
+},];
+
+const cols = [{
+  title: '#ID',
+  dataIndex: 'id',
+  key: 'id',
+}, {
+  title: 'Date',
+  dataIndex: 'date',
+  key: 'date',
+}, {
+  title: 'Topic title',
+  dataIndex: 'title',
+  key: 'title',
+}, {
+  title: 'Action',
+  dataIndex: 'action',
+  key: 'action',
+  render: (text, record) => (
+    <span>
+      <a href="#">Action 一 {record.name}</a>
+      <span className="ant-divider" />
+      <a href="#">Delete</a>
+      <span className="ant-divider" />
+      <a href="#" className="ant-dropdown-link">
+        More actions <Icon type="down" />
+      </a>
+    </span>
+  ),
+}];
+
+const selection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    //console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  onSelect: (record, selected, selectedRows) => {
+    //console.log(record, selected, selectedRows);
+  },
+  onSelectAll: (selected, selectedRows, changeRows) => {
+    //console.log(selected, selectedRows, changeRows);
+  },
+  getCheckboxProps: record => ({
+    //disabled: record.name === 'Disabled User',    // Column configuration not to be checked
+  }),
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={titleimg} className="App-titleimg" alt="titleimg" />
+          <p className="App-title">Thesis Portal</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          What about no.
-        </p>
+        <div className="App-body">
+          <div className="App-body-table">
+           <Table
+            rowSelection={selection}
+            expandedRowRender={record => <p>{record.description}</p>}
+            dataSource={data}
+            columns={cols}
+            size="middle"
+            pagination={ false }
+            //scroll={{ y: 240 }}
+            bordered />
+          </div>
+        </div>
       </div>
+
     );
   }
 }
