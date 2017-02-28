41c5dc93f0ee00db4eb04c5f6e2d53120d0fefb9
PSilling <petr.silling@centrum.cz>
2017-02-28 23:49:14 +0100

    Update
    First release with working network transfering and simple response data viewing.

--- src/App.js
+++ src/App.js
@@ -58,6 +58,61 @@ const selection = {
   }),
 };
 
+//Functions for data transffer between the client and the server.
+function get() {
+  return fetch('/topics', {
+    method: 'get'
+  }).then(function(response) {
+      if(response.ok) {
+        return response.json();
+    } else throw new Error('There was a problem with network connection.');
+  }).then(function(json) {
+      let d = new Date(json[0].created);
+      document.getElementById('get').innerHTML = 'Got data! '+'ID: '+json[0].id+' Title: '+json[0].title+' Description: '+json[0].Description+ ' Created: '+d.toUTCString();
+  });
+}
+
+function post() {
+  var date = new Date();
+  return fetch('/topics', {
+    method: 'post',
+    headers: { "Content-Type" : "application/json" },
+    body: JSON.stringify({id:'1', title:'Posted topic', Description: 'Abutere...', created: date})
+  }).then(function(response) {
+      if(response.ok) {
+        return response.json();
+    } else throw new Error('There was a problem with network connection.');
+  }).then(function(json) {
+      let d = new Date(json.created);
+      document.getElementById('post').innerHTML = 'Data sent successfully! '+'ID: '+json.id+' Title: '+json.title+' Description: '+json.Description+ ' Created: '+d.toUTCString();
+  });
+}
+
+function put() {
+  var date = new Date();
+  return fetch('/topics/0', {
+    method: 'put',
+    headers: { "Content-Type" : "application/json" },
+    body: JSON.stringify({id:'1', title:'Posted topic', Description: 'Abutere...', created: date})
+  }).then(function(response) {
+      if(response.ok) {
+        return response.json();
+    } else throw new Error('There was a problem with network connection.');
+  }).then(function(json) {
+      let d = new Date(json.created);
+      document.getElementById('put').innerHTML = 'Data updated! '+'ID: '+json.id+' Title: '+json.title+' Description: '+json.Description+ ' Created: '+d.toUTCString();
+  });
+}
+
+function del() {
+  return fetch('/topics/0', {
+    method: 'delete'
+  }).then(function(response) {
+      if(response.ok) document.getElementById('del').innerHTML = 'Data deleted! '+response.status;
+    else throw new Error('There was a problem with network connection.')
+    });
+}
+
 class App extends Component {
   render() {
     return (
@@ -77,6 +132,10 @@ class App extends Component {
             pagination={ false }
             //scroll={{ y: 240 }}
             bordered />
+            <Button type="primary" id="get" onClick={get}>GET</Button>
+            <Button type="primary" id="post" onClick={post}>POST</Button>
+            <Button type="primary" id="put" onClick={put}>PUT</Button>
+            <Button type="primary" id="del" onClick={del}>DELETE</Button>
           </div>
         </div>
       </div>
